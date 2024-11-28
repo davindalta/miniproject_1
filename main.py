d9fadcbe51ecd715d33c5d2a33cce92cdb532641
Yeremi Andrew <kurikarakenjou@gmail.com>
2024-11-28 19:35:50 +0700

    -

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ if __name__ == "__main__":
     conn, engine = connection.get_conn(conf, "marketplace_prod")
     cursor = conn.cursor()
 
-    cursor.execute("CREATE SCHEMA IF NOT EXISTS dwh;")
-    conn.commit()
-
     # connection dwh
     conf_dwh = connection.config("dwh", "config.json")
     conn_dwh, engine_dwh = connection.get_conn(conf_dwh, "dwh")
     cursor_dwh = conn_dwh.cursor()
+
+    # create schema inside dwh database
+    schema_name = "dwh_kelompok_2"
+    cursor_dwh.execute("CREATE SCHEMA IF NOT EXISTS %s;" % schema_name)
+    conn_dwh.commit()
 
     # get query string
     path_query = os.path.join(os.getcwd(), "query")
@@ -24,7 +26,8 @@ if __name__ == "__main__":
         open(os.path.join(path_query, "query.sql"), "r").read(), strip_comments=True
     ).strip()
     dwh_design = sqlparse.format(
-        open(os.path.join(path_query, "dwh_design.sql"), "r").read(), strip_comments=True
+        open(os.path.join(path_query, "dwh_design.sql"), "r").read(),
+        strip_comments=True,
     ).strip()
 
     try:
@@ -32,7 +35,6 @@ if __name__ == "__main__":
 
         # get data from source (schema = public, if your source schema is not public, then adjust the query to use with your source schema)
         df = pd.read_sql(query, engine)
-        
 
         # insert the schema into dwh schema
         cursor_dwh.execute(dwh_design)
@@ -40,10 +42,14 @@ if __name__ == "__main__":
 
         # ingest data into dwh schema
         df.to_sql(
-            "dim_orders", engine_dwh, schema="public", if_exists="replace", index=False
+            "dim_orders",
+            engine_dwh,
+            schema=schema_name,
+            if_exists="replace",
+            index=False,
         )
 
         print("[INFO] service etl is success")
     except Exception as e:
-        print("[INFO] service etl is failed")
+        print("[ERROR] service etl is failed")
         print(str(e))
